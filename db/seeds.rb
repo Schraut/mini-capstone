0604151251cc6f6bed2a26356fcb03388c502034
Dan Shrout <danshrout@yahoo.com>
2017-11-30 18:34:54 -0600

    add three products to seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product_1 = Product.new(name: "toothbrush", price: 2, image: "", description: "It cleans teeth")
+
+product_2 = Product.new(name: "camera", price: 5, image: "", description: "Takes pictures")
+
+roduct_3 = Product.new(name: "brush", price: 4, image: "", description: "Used to style hair")
+#name, price, image, description
+product_1.save
+product_2.save
+product_3.save
